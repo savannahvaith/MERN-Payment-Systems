95639384a5a34b4dc54e64987fc6d29f8a3ea025
savannahvaith <sav.vaithilingam@gmail.com>
2021-03-08 11:00:54 +0000

    SV: added stripe container

---
+++ frontend/src/Components/Checkout/StripeContainer.jsx
@@ -0,0 +1,17 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import {PUBLISHABLE_KEY} from '../../CONSTS.json';
+import { CheckoutForm } from "./CheckoutForm";
+
+const stripePromise = loadStripe(`${PUBLISHABLE_KEY}`);
+
+
+const Stripe = () => {
+    return (
+        <Elements stripe={stripePromise}>
+            <CheckoutForm />
+        </Elements>
+    );
+};
+
+export default Stripe;
