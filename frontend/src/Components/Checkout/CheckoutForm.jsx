0e0eaa442d222aa7302c536b3a39168a155ec2b5
savannahvaith <sav.vaithilingam@gmail.com>
2021-03-08 16:25:25 +0000

    SV: Moved images

--- frontend/src/Components/Checkout/CheckoutForm.jsx
+++ frontend/src/Components/Checkout/CheckoutForm.jsx
@@ -2,12 +2,14 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import axios from 'axios';
 import { Icon, Step, Grid, Segment } from 'semantic-ui-react'
 import { BACKEND_URL } from '../../CONSTS.json';
-import "@repay/react-credit-card/dist/react-credit-card.css";
+import { useState } from 'react';
 import MultiStepForm from "./Multistep/MultiStepForm";
 
 export const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+
+    const [step, setStep] = useState(1);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -40,55 +42,155 @@ export const CheckoutForm = () => {
         }
     };
 
-
     return (
         <div className="container">
             <div className="row">
                 <br />
                 <Step.Group>
-                    <Step completed>
-                        <Icon name='cart' />
-                        <Step.Content>
-                            <Step.Title>Cart</Step.Title>
-                            <Step.Description>Review your cart</Step.Description>
-                        </Step.Content>
-                    </Step>
-                    <Step>
-                        <Icon name='truck' />
-                        <Step.Content>
-                            <Step.Title>Shipping</Step.Title>
-                            <Step.Description>Choose your shipping options</Step.Description>
-                        </Step.Content>
-                    </Step>
+                    {step == 1 &&
+                        <>
+                            <Step completed>
+                                <Icon name='cart' />
+                                <Step.Content>
+                                    <Step.Title>Cart</Step.Title>
+                                    <Step.Description>Review your cart</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step active >
+                                <Icon name='truck' />
+                                <Step.Content>
+                                    <Step.Title>Shipping</Step.Title>
+                                    <Step.Description>Choose your shipping options</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step  >
+                                <Icon name='payment' />
+                                <Step.Content>
+                                    <Step.Title>Billing</Step.Title>
+                                    <Step.Description>Enter billing information</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step >
+                                <Icon name='info' />
+                                <Step.Content>
+                                    <Step.Title>Confirm Order</Step.Title>
+                                </Step.Content>
+                            </Step>
+                        </>
+                    }
 
-                    <Step >
-                        <Icon name='payment' />
-                        <Step.Content>
-                            <Step.Title>Billing</Step.Title>
-                            <Step.Description>Enter billing information</Step.Description>
-                        </Step.Content>
-                    </Step>
 
-                    <Step>
-                        <Icon name='info' />
-                        <Step.Content>
-                            <Step.Title>Confirm Order</Step.Title>
-                        </Step.Content>
-                    </Step>
+                    {
+                        step == 2 &&
+                        <>
+                            <Step completed>
+                                <Icon name='cart' />
+                                <Step.Content>
+                                    <Step.Title>Cart</Step.Title>
+                                    <Step.Description>Review your cart</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step completed>
+                                <Icon name='truck' />
+                                <Step.Content>
+                                    <Step.Title>Shipping</Step.Title>
+                                    <Step.Description>Choose your shipping options</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step active>
+                                <Icon name='payment' />
+                                <Step.Content>
+                                    <Step.Title>Billing</Step.Title>
+                                    <Step.Description>Enter billing information</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step >
+                                <Icon name='info' />
+                                <Step.Content>
+                                    <Step.Title>Confirm Order</Step.Title>
+                                </Step.Content>
+                            </Step>
+                        </>
+                    }
+
+                    {step == 3 &&
+                        <>
+                        <Step completed>
+                            <Icon name='cart' />
+                            <Step.Content>
+                                <Step.Title>Cart</Step.Title>
+                                <Step.Description>Review your cart</Step.Description>
+                            </Step.Content>
+                        </Step>
+                            <Step completed>
+                                <Icon name='truck' />
+                                <Step.Content>
+                                    <Step.Title>Shipping</Step.Title>
+                                    <Step.Description>Choose your shipping options</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step completed >
+                                <Icon name='payment' />
+                                <Step.Content>
+                                    <Step.Title>Billing</Step.Title>
+                                    <Step.Description>Enter billing information</Step.Description>
+                                </Step.Content>
+                            </Step>
+                        <Step active>
+                            <Icon name='info' />
+                            <Step.Content>
+                                <Step.Title>Confirm Order</Step.Title>
+                            </Step.Content>
+                        </Step>
+                        </>
+                    }
+                    {step == 4 &&
+                        <>
+                        <Step completed>
+                            <Icon name='cart' />
+                            <Step.Content>
+                                <Step.Title>Cart</Step.Title>
+                                <Step.Description>Review your cart</Step.Description>
+                            </Step.Content>
+                        </Step>
+                        <Step completed>
+                            <Icon name='truck' />
+                            <Step.Content>
+                                <Step.Title>Shipping</Step.Title>
+                                <Step.Description>Choose your shipping options</Step.Description>
+                            </Step.Content>
+                        </Step>
+                            <Step completed >
+                                <Icon name='payment' />
+                                <Step.Content>
+                                    <Step.Title>Billing</Step.Title>
+                                    <Step.Description>Enter billing information</Step.Description>
+                                </Step.Content>
+                            </Step>
+                            <Step completed>
+                                <Icon name='info' />
+                                <Step.Content>
+                                    <Step.Title>Confirm Order</Step.Title>
+                                </Step.Content>
+                            </Step>
+                        </>
+                    }
+
+
                 </Step.Group>
             </div>
             {/* <Segment placeholder> */}
             <Grid columns={2} relaxed='very' stackable divided inverted padded>
                 <Grid.Column width={13} >
                     <div className="container">
-                        <MultiStepForm/>
+                        <MultiStepForm step={step} setStep={setStep} />
                     </div>
                 </Grid.Column>
 
                 <Grid.Column width={3} >
                     <Segment color="black">
                         <p>Hello</p>
-                        <hr/>
+                        <hr />
                         <p>$</p>
                     </Segment>
                 </Grid.Column>
