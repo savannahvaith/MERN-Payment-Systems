c1fdc1c41f49313e9e6d38237b33efabe8ece5d5
savannahvaith <sav.vaithilingam@gmail.com>
2021-03-08 11:02:43 +0000

    SV: added product context

---
+++ frontend/src/Context/ProductsContext.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import React, { createContext, useState, useEffect } from 'react';
+export const ProductsContext = createContext()
+
+const ProductsContextProvider = ({ children }) => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect( () => {
+        axios.get(`http://localhost:5019/product/getAll`)
+        .then( (res) => setProducts(res.data))
+        .catch( (err) => console.error(err))
+    })
+
+    return (
+        <ProductsContext.Provider value={{ products }} >
+            { children}
+        </ProductsContext.Provider>
+    );
+}
+
+export default ProductsContextProvider;
