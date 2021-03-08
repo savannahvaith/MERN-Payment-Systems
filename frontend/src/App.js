396d19ede8ea4b53229066f06ede417687c98cf5
savannahvaith <sav.vaithilingam@gmail.com>
2021-03-08 10:58:35 +0000

    SV: Added routes for navigation of site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+import NavigationBar from './Components/NavigationBar';
+import Store from './Components/Store/Store';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Details from './Components/Details';
+import Cart from './Components/Cart/Cart';
+import Stripe from './Components/Checkout/StripeContainer';
+import PaymentDetails from './Components/Checkout/Multistep/PaymentDetails';
 function App() {
   return (
     <>
-    <p>Welcome to my website!</p>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route path="/" exact>
+            <Store/>
+          </Route>
+          <Route path="/Basket">
+            <Cart/>
+          </Route>
+          <Route path="/Checkout">
+            <Stripe />
+          </Route>
+          <Route path="/details/:id">
+            <Details/>
+          </Route>
+        </Switch>
+      </Router>
     </>
-  );
+  )
 }
-
-export default App;
+export default App; 
