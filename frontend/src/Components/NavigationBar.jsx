b3f8af5b65fec4f4a09f121ed6d696b68b0bc981
savannahvaith <sav.vaithilingam@gmail.com>
2021-03-10 10:56:02 +0000

    SV: Updated Navigation bar

--- frontend/src/Components/NavigationBar.jsx
+++ frontend/src/Components/NavigationBar.jsx
@@ -1,11 +1,12 @@
 import { useContext, useState } from 'react';
 import { Navbar, Nav, Collapse, NavItem, NavLink, NavbarBrand, NavbarToggler, NavbarText } from 'reactstrap';
+import { Icon, Label, Menu } from 'semantic-ui-react';
 import { CartContext } from '../Context/CartContext';
 const NavigationBar = (props) => {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
-    const {itemCount} = useContext(CartContext);
+    const { itemCount } = useContext(CartContext);
 
     return (
         <div>
@@ -18,11 +19,18 @@ const NavigationBar = (props) => {
                             <NavLink href="https://github.com/savannahvaith/MERN-Stripe">GitHub</NavLink>
                         </NavItem>
                     </Nav>
-                    <NavLink href="/Basket"> Cart ({itemCount})</NavLink>
-                    <NavbarText>Savannah Vaithilingam</NavbarText>
+                    <NavLink href="/Basket">
+                        <Menu compact>
+                            <Menu.Item as='a'>
+                            <Icon name="shopping basket icon" size="large" /> 
+                                <Label color="teal" floating> ({itemCount})
+                                </Label>
+                            </Menu.Item>
+                        </Menu>
+                    </NavLink>
                 </Collapse>
             </Navbar>
         </div>
     )
 }
-export default NavigationBar; 
+export default NavigationBar;
